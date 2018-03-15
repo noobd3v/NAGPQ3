22c88e956ff423893f8ba2970452cb6ef93b293a
Abhishek Chandel <abhishek.chandel@nagarro.com>
2018-03-15 14:57:34 +0530

    adding files

---
+++ DevopsNagpQ3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevopsNagpQ3.Startup))]
+namespace DevopsNagpQ3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevopsNagpQ3.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            HomeController ctr = new HomeController();
+            var ar = ctr.About() as ViewResult;
+            Assert.AreEqual("Nagp q3 test", ar.ViewData["Message"]);
+        }
+    }
+}
